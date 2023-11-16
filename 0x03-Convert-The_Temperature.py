d22b532109b8f676ebb8d4baa61eb3a8b090d441
Abel Yitages Demissie <99829666+kiya3300@users.noreply.github.com>
2023-11-16 13:05:19 +0300

    A2SV-DSA

---
+++ 0x03-Convert-The_Temperature.py
@@ -0,0 +1,9 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kalvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        return [kalvin, fahrenheit]
+
+        celsius = 36.50
+        ans = convertTemperature(celsius)
+        print(ans)
